4cb101e688721cbbccd7ec85ef833db7181eb390
SevakMat <sevakmatevosyan98@gmail.com>
2023-12-22 20:58:52 +0400

    integrated rims

---
+++ src/components/shere/tires/TiresContent.tsx
@@ -0,0 +1,49 @@
+import { Box, Button, List } from "@mui/material";
+import { TireType } from "../../../store/types/tire/tire";
+import TyreCard from "./TireCard";
+
+interface TyresListProps {
+  tiresList: TireType[]
+}
+
+const TiresContent = ({ tiresList }: TyresListProps) => {
+
+  return (
+    <>
+      <h2>Tires !!!</h2>
+      <Box>
+        <List
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "space-between",
+            rowGap: 3,
+            "@media (max-width: 1090px)": {
+              alignItems: "flex-start",
+              display: "flex",
+              flexWrap: "nowrap",
+              listStyle: "none",
+              margin: "1rem 0",
+              overflowX: "scroll",
+              padding: "0 0 0 1rem",
+              gap: 3,
+            },
+          }}
+        >
+          {tiresList.map((tire: TireType, index: number) => {
+            return (
+              <li>
+                <TyreCard
+                  tire={tire}
+                />
+              </li>
+            );
+          })}
+        </List>
+      </Box>
+
+    </>
+  );
+};
+
+export default TiresContent;
---
+++ src/store/reducers/tire/tire.ts
@@ -0,0 +1,26 @@
+import { Reducer } from "redux";
+import { GetTireListSuccess, TireActionTypes, TireServerTypes, TireState } from "../../types/tire/tire";
+
+const initialState: TireState = {
+  tireList: [],
+}
+
+type ReducerType = Reducer<TireState, TireActionTypes>
+
+
+const reducer: ReducerType = (state = initialState, action: GetTireListSuccess) => {
+
+  switch (action.type) {
+    case TireServerTypes.GET_TIRE_LIST_SUCCESS:
+      return {
+        ...state,
+        tireList: action.tireList,
+      };
+
+    default:
+      return state;
+  }
+};
+
+
+export default reducer;
