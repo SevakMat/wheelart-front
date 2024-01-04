cd8b435dd4bbb352978969968bbf937996115e01
SevakMat <sevakmatevosyan98@gmail.com>
2024-01-04 23:26:58 +0400

    added  language translation part

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,33 @@ import { ToastContainer } from 'react-toastify';
 import RoutersContainer from './routers/RoutersContainer';
 import 'react-toastify/dist/ReactToastify.css';
 
+import global_en from "./translations/en/global.json"
+import global_fr from "./translations/fr/global.json"
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+
+i18next.init({
+  interpolation: { escapeValue: false },
+  lng: "en",
+  resources: {
+    en: {
+      global: global_en
+    },
+    fr: {
+      global: global_fr
+    }
+  }
+})
+
+
+
 function App() {
   return (
-    <>
+    <I18nextProvider i18n={i18next}>
       <RoutersContainer />
       <ToastContainer />
-    </>
-    );
+    </I18nextProvider>
+  );
 }
 
 export default App;
