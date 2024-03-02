fddb9a40f253e3bf19136583a2021d634d5f7fef
vardan <vstepanyan17@gmail.com>
2024-03-02 20:09:51 +0400

    ContactUs page is ready (not connected to the backend)

---
+++ src/pages/ContactUsPage.tsx
@@ -0,0 +1,12 @@
+import MainContainer from "../hok/mainContainer/MainContainer";
+import ContactUsContainer from "../components/contactUsContainer/ContactUsContainer";
+
+const ContactUsPage = () => {
+  return (
+    <MainContainer>
+      <ContactUsContainer />
+    </MainContainer>
+  );
+};
+
+export default ContactUsPage;
