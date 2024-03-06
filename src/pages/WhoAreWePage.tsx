08aa6eb475619f20ab34b85010dc50cdd9a34aec
vardan <vstepanyan17@gmail.com>
2024-03-06 20:59:20 +0400

    WhoAreWe is created

---
+++ src/pages/WhoAreWePage.tsx
@@ -0,0 +1,12 @@
+import WhoAreWeContainer from "../components/whoAreWeContainer/WhoAreWeContainer";
+import MainContainer from "../hok/mainContainer/MainContainer";
+
+const WhoAreWePage = () => {
+  return (
+    <MainContainer>
+      <WhoAreWeContainer />
+    </MainContainer>
+  );
+};
+
+export default WhoAreWePage;
